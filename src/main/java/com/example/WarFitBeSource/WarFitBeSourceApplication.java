04adad7ded1c6da0c4735e5842b6a505e0a0f0cb
lele9494 <cagnina10@gmail.com>
2025-01-31 14:53:46 +0100

    Add Be Project

---
+++ src/main/java/com/example/WarFitBeSource/WarFitBeSourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.WarFitBeSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarFitBeSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarFitBeSourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WarFitBeSource/WarFitBeSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WarFitBeSource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarFitBeSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
